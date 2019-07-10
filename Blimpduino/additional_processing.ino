3e8aae87f46974916b74da04cf61035780b4e80f
Andersound <fa8@kth.se>
2019-07-10 11:02:19 +0200

    Added files for further development

---
+++ Blimpduino/additional_processing.ino
@@ -0,0 +1,3 @@
+// File for definition of additional processing functions
+// -Fusing of pressure sensor + range sensor +  filtering for height estimation
+// -Controller
---
+++ Blimpduino/rpi_comm.ino
@@ -0,0 +1,2 @@
+// Functionality for i2c communication with a rpi configured as i2c slave
+// Read/Write from/to rpi
